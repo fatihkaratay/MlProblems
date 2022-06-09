712899fea67707e9533c839a5859985b55aeab7b
Fatih Karatay <fatih.karatay@ngc.com>
2022-06-09 15:50:22 -0400

    compact matrix is computed

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -17,3 +17,23 @@ for i in range(4):
         if sparseMatrix[i][j] != 0:
             size += 1
 print('compact matrix size is ', size)
+
+'''
+number of columns in the compact matrix should be equal to size of non zero
+elements in the sparse matrix
+'''
+
+rows, cols = (3, size)
+
+compact_matrix = [[0 for i in range(cols)] for j in range(rows)]
+
+k = 0
+for i in range(4):
+    for j in range(5):
+        if sparseMatrix[i][j] != 0:
+            compact_matrix[0][k] = i
+            compact_matrix[1][k] = j
+            compact_matrix[2][k] = sparseMatrix[i][j]
+            k += 1
+
+print(compact_matrix)
