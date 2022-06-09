87ed38913d4d4fe965d2d17b16280a70b90d9922
Fatih Karatay <fatih.karatay@ngc.com>
2022-06-09 15:46:42 -0400

    compact matrix size is computed

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -8,3 +8,12 @@ print('sparse_matrix representation')
 # now store the value,row,column of arr1 in sparse matrix compactMatrix
 
 sparseMatrix = [[0, 0, 3, 0, 4], [0, 0, 5, 7, 0], [0, 0, 0, 0, 0], [0, 2, 6, 0, 0]]
+
+# initialize the size as 0
+size = 0
+
+for i in range(4):
+    for j in range(5):
+        if sparseMatrix[i][j] != 0:
+            size += 1
+print('compact matrix size is ', size)
