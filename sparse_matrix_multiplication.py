bc7171a9053ad19d93689602e181893f78bda502
Fatih Karatay <fatih.karatay@ngc.com>
2022-06-09 16:37:55 -0400

    sparse matrix multiplication is completed

--- sparse_matrix_multiplication.py
+++ sparse_matrix_multiplication.py
@@ -33,7 +33,29 @@ matrix_b = [
     [0, 0, 4]
 ]
 
+
 def sparse_matrix_multiplication(matrix_a, matrix_b):
-    return [[]]
+    if len(matrix_a[0]) != len(matrix_b):
+        return [[]]
+    sparse_a = get_dict_of_nonzero_cells(matrix_a)
+    sparse_b = get_dict_of_nonzero_cells(matrix_b)
+
+    res_matrix = [[0] * len(matrix_b[0]) for _ in range(len(matrix_a))]
+
+    for i, k in sparse_a.keys():
+        for j in range(len(matrix_b[0])):
+            if (k, j) in sparse_b.keys():
+                res_matrix[i][j] += sparse_a[(i, k)] * sparse_b[(k, j)]
+    return res_matrix
+
+
+def get_dict_of_nonzero_cells(matrix):
+    dict_of_nonzero_cells = {}
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] != 0:
+                dict_of_nonzero_cells[(i, j)] = matrix[i][j]
+    return dict_of_nonzero_cells
+
 
 print(sparse_matrix_multiplication(matrix_a, matrix_b))
