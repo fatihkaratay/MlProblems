d40a8958792bfe55685fc5c5e930048c7face963
Fatih Karatay <fatih.karatay@ngc.com>
2022-06-09 15:35:06 -0400

    Initial commit

---
+++ sparse_matrix.py
@@ -0,0 +1,10 @@
+print('sparse_matrix representation')
+
+# Python program for Sparse Matrix Representation
+# using arrays
+
+# assume a sparse matrix of order 4*5
+# let assume another matrix compactMatrix
+# now store the value,row,column of arr1 in sparse matrix compactMatrix
+
+sparseMatrix = [[0, 0, 3, 0, 4], [0, 0, 5, 7, 0], [0, 0, 0, 0, 0], [0, 2, 6, 0, 0]]
