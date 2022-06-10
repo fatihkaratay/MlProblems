52395406ba08f630d7f41f2bb534b0015d71b323
fatihkaratay <fatihkaratay@gmail.com>
2022-06-10 01:18:08 -0400

    k-means is added

---
+++ k_means.py
@@ -0,0 +1,11 @@
+'''
+initialize centroids with ransom 2 points
+for num_iterations:
+    for user in data:
+        for centroid in centroids:
+            min_list = centroid.add(user)
+            user, centroid
+    for centroid in centroids:
+        update_centroid_locations
+return centroid_locations
+'''
