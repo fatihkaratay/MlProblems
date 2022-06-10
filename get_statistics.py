34d379bf62b1a7d843b446e037c00e2c867c8a80
fatihkaratay <fatihkaratay@gmail.com>
2022-06-09 23:54:22 -0400

    mean is calculated

--- get_statistics.py
+++ get_statistics.py
@@ -11,3 +11,26 @@ MEAN CONFIDENCE INTERVAL=
     3. lower and upper values are substract of add from median respectively.
         = [mean-z-score*standard_err. mean+z-score*standard_err]
 '''
+
+input_list = [2, 1, 3, 4, 4, 5, 6, 7]
+
+
+def get_statistics(input_list):
+    sorted_list = sorted(input_list)
+    input_len = len(input_list)
+
+    mean = sum(sorted_list) / input_len
+
+
+
+    return {
+        "mean": mean,
+        "median": 0,
+        "mode": 0,
+        "sample_variance": 0,
+        "sample_standard_deviation": 0,
+        "mean_confidence_interval": [0, 0]
+    }
+
+
+print(get_statistics(input_list))
