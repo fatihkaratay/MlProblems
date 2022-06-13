2a6b1a6c6cc5a880c03e79f634057ec56956cd3b
Fatih Karatay <fatih.karatay@ngc.com>
2022-06-13 16:37:30 -0400

    neuron initial code is computed

--- neuron.py
+++ neuron.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -15,10 +17,35 @@ class Neuron:
     # Use the sigmoid activation function.
     # Use the defaults for the function arguments.
     def train(self, learning_rate=0.01, batch_size=10, epochs=200):
-        # Write your code here.
-        pass
+        for _ in range(epochs):
+            for batch_window in range(len(self.examples) // batch_size):
+                mini_batch = self.examples[0 + (batch_size * batch_window) : batch_size + (batch_size * batch_window)]
+                prediction_labels = [
+                    {"prediction": self.predict(example["features"]), "label": example["label"]}
+                    for example in mini_batch
+                ]
+                gradients = self.__get_gradients(mini_batch, prediction_labels)
+                self.weights = self.weights - [learning_rate * gradient for gradient in gradients]
 
     # Return the probability—not the corresponding 0 or 1 label.
     def predict(self, features):
-        # Write your code here.
-        pass
+        model_inputs = features + [1]
+        wTx = 0
+        for i, model_input in enumerate(model_inputs):
+            wTx = wTx + self.weights[i] * model_input
+        return 1 / (1 + math.exp(-wTx))
+
+
+    def __get_gradients(self, batch, prediction_labels):
+        errors = [
+            prediction_label["prediction"] - prediction_label["label"] for prediction_label in prediction_labels
+        ]
+
+        gradients = [0] * len(self.weights)
+        for example_i, example in enumerate(batch):
+            features = example["features"] + [1]
+            for feature_i, feature in enumerate(features):
+                gradients[feature_i] = gradients[feature_i] + errors[example_i] * feature
+        gradients = [gradient / len(batch) for gradient in gradients]
+        return gradients
+
